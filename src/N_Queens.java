bad0ece217c5159a1b04cff6c32a893a7c583548
TaufeeqNoamaan <89037322+TaufeeqNoamaan@users.noreply.github.com>
2024-08-05 18:42:52 +0530

    Fixed N-Queens

--- src/N_Queens.java
+++ src/N_Queens.java
@@ -2,12 +2,12 @@ public class N_Queens {
         int n;
         int x[];
 
-        public N_Queens() {
+        public N_Queens(int n) {
             this.n = n;
             x = new int[n];
         }
         public void queens(int k, int n) {
-            for (int i = 0; i <= n; i++) {
+            for (int i = 0; i < n; i++) {
                 if (place(k, i)) {
                     x[k] = i;
                     if(k == n-1) {
@@ -21,7 +21,7 @@ public class N_Queens {
         }
 
     public boolean place(int k, int i) {
-        for (int j = 0; j <= k; j++) {
+        for (int j = 0; j < k; j++) {
             if (x[j] == i || Math.abs(x[j] - i) == Math.abs(x[j] - k)) {
                 return false;
             }
@@ -30,7 +30,7 @@ public class N_Queens {
         return true;
     }
     public void display() {
-            for (int i = 0; i <= n; i++) {
+            for (int i = 0; i < n; i++) {
                 System.out.println(x[i] + "\t");
         }
         System.out.println();
@@ -38,7 +38,7 @@ public class N_Queens {
 
     public static void main(String[] args) {
         int n = 4;
-        N_Queens q = new N_Queens();
+        N_Queens q = new N_Queens(n);
         q.queens(0,n);
     }
 }
