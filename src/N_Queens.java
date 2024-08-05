6cc0e2dfb701280d5f14c90e68c42ab52892adea
TaufeeqNoamaan <89037322+TaufeeqNoamaan@users.noreply.github.com>
2024-08-05 18:36:19 +0530

    Added N-Queens, persisting error

---
+++ src/N_Queens.java
@@ -0,0 +1,44 @@
+public class N_Queens {
+        int n;
+        int x[];
+
+        public N_Queens() {
+            this.n = n;
+            x = new int[n];
+        }
+        public void queens(int k, int n) {
+            for (int i = 0; i <= n; i++) {
+                if (place(k, i)) {
+                    x[k] = i;
+                    if(k == n-1) {
+                        display();
+                    }
+                    else {
+                        queens(k+1, n);
+                    }
+                }
+            }
+        }
+
+    public boolean place(int k, int i) {
+        for (int j = 0; j <= k; j++) {
+            if (x[j] == i || Math.abs(x[j] - i) == Math.abs(x[j] - k)) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+    public void display() {
+            for (int i = 0; i <= n; i++) {
+                System.out.println(x[i] + "\t");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        N_Queens q = new N_Queens();
+        q.queens(0,n);
+    }
+}
